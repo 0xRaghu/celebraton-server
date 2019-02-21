2627553ebb5887bbfcb9b056738c0ca210b877fa
Shri Raghu Raaman <shriraghuraman@gmail.com>
2019-02-21 10:52:29 +0530

    deploying server to heroku

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ app.use(cors());
 
 app.use(function(req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "https://celebraton.now.sh");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://celebraton.herokuapp.com"
+  );
 
   // Request methods you wish to allow
   res.setHeader(
