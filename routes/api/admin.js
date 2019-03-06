adb52c15dfb83b73d55837e9a45dcb85e97f4d90
Shri Raghu Raaman <shriraghuraman@gmail.com>
2019-03-06 11:16:50 +0530

    Added sitemap functionlity for SEO

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -211,9 +211,6 @@ router.post(
   "/adminManageEnquiry",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    if(req.user.role==="admin"){
-
-    
     let budgetRange = { from: 0, to: 0, option: 0 };
     let leadAmount;
     const enquiry = req.body.enquiry;
@@ -333,9 +330,6 @@ router.post(
       )
 
       .catch(err => console.log(err));
-      }else{
-      res.status(401).json({message:"Not Authorized"});
-    }
   }
 );
 
@@ -343,9 +337,6 @@ router.post(
   "/adminManageProfile",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    if(req.user.role==="admin"){
-
-    
     const profile = req.body.values;
     const mode = req.body.mode;
 
@@ -522,10 +513,6 @@ router.post(
       )
 
       .catch(err => console.log(err));
-      
-      }else{
-        res.status(401).json({message:"Not Authorized"});
-      }
   }
 );
 
